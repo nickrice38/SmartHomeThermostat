308990a55bcc19814c6506d345eda607314e231f
Nick Rice <nick.rice@starlingbank.com>
2022-05-24 17:37:43 +0100

    Initial Commit

---
+++ SmartHomeThermostat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartHomeThermostat
+//
+//  Created by Nick Rice on 24/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartHomeThermostat/SmartHomeThermostatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeThermostatApp.swift
+//  SmartHomeThermostat
+//
+//  Created by Nick Rice on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeThermostatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
